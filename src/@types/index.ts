95fa023a0108e8d2e0cd86b0d088adf92d451880
Linc <762699299@qq.com>
2025-03-01 22:43:03 +0800

    ♻️ 🎨 refactor: simplify editing of style files (#89)
    * refactor: simplify editing of style files
    * ⬆️ ➕  Add sass, bump versions
    * 🎨  update css (built by sass)
    * 🔧 👷 use build tasks to build sass
    * 📝  biome lint/format; CONTRIBUTING.md
    
    ---------
    
    Co-authored-by: Erin Schnabel <erinschnabel@gmail.com>

--- src/@types/index.ts
+++ src/@types/index.ts
@@ -3,7 +3,7 @@ export interface MdItObsidianCalloutsOptions {
      * CSS language prefix for fenced blocks.
      * Can be useful for external highlighters.
      */
-    langPrefix?: string | '';
+    langPrefix?: string | "";
 
     /**
      * Custom icons for each marker. The key is the marker name, and the value is the html script represent the icon.
@@ -11,5 +11,5 @@ export interface MdItObsidianCalloutsOptions {
      *
      * @default inline svg icons from Obsidian (lucide) or the note icon if not found
      */
-    icons?: Record<string, string>
+    icons?: Record<string, string>;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,43 @@
 // Import the MarkdownIt type from 'markdown-it'
-import MarkdownIt from 'markdown-it'
-import { MdItObsidianCalloutsOptions } from './@types';
-import { inspectBlockquoteContent, inspectFencedCodeContent, renderCalloutPostfix, renderCalloutPrefix } from './inspect';
+import type MarkdownIt from "markdown-it";
+import type { MdItObsidianCalloutsOptions } from "./@types";
+import {
+    inspectBlockquoteContent,
+    inspectFencedCodeContent,
+    renderCalloutPostfix,
+    renderCalloutPrefix,
+} from "./inspect";
 
 // Define your plugin
-export default function mdItObsidianCallouts(md: MarkdownIt, options: MdItObsidianCalloutsOptions = {}): void {
-    md.core.ruler.after('block', 'obsidian-callouts', (state) => {
+export default function mdItObsidianCallouts(
+    md: MarkdownIt,
+    options: MdItObsidianCalloutsOptions = {},
+): void {
+    md.core.ruler.after("block", "obsidian-callouts", (state) => {
         const tokens = state.tokens;
         for (let i = 0; i < tokens.length; i++) {
             const token = tokens[i];
-            if (token.type === 'blockquote_open') {
+            if (token.type === "blockquote_open") {
                 inspectBlockquoteContent(tokens, i);
             }
-            if (token.type === 'fence') {
+            if (token.type === "fence") {
                 inspectFencedCodeContent(tokens, i, md, options);
             }
         }
     });
 
-    md.renderer.rules.callout_open = function (tokens, idx) {
+    md.renderer.rules.callout_open = (tokens, idx) => {
         const token = tokens[idx];
         return renderCalloutPrefix(token, md, options);
-    }
+    };
 
-    md.renderer.rules.admonition_block = function (tokens, idx) {
+    md.renderer.rules.admonition_block = (tokens, idx) => {
         const token = tokens[idx];
-        return renderCalloutPrefix(token, md, options)
-            + `${token.content}\n</div>\n</div>`;
-    }
+        return `${renderCalloutPrefix(token, md, options)}${token.content}\n</div>\n</div>`;
+    };
 
-    md.renderer.rules.callout_close = function (tokens, idx) {
+    md.renderer.rules.callout_close = (tokens, idx) => {
         const token = tokens[idx];
         return renderCalloutPostfix(token, options);
-    }
+    };
 }
